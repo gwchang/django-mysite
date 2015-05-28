137dfa48a6de6739a6cd089b9a29872b1425b990
Gary Chang <gwh.chang@gmail.com>
2015-05-28 15:13:12 -0500

    populate script

--- curations/management/commands/populate_db.py
+++ curations/management/commands/populate_db.py
@@ -1,2 +1,14 @@
-from django.core.manage.base import BaseCommand
-from fancy.models import 
+from django.core.management.base import BaseCommand, CommandError
+from curations.models import Collection, Asset
+
+class Command(BaseCommand):
+  help = 'Populates Collection, Asset db from yaml file'
+  
+  def add_arguments(self, parser):
+    parser.add_argument('yaml_file', type=str)
+
+  def handle(self, *args, **options):
+    yaml_file = options['yaml_file']
+    msg = 'Parsing yaml file %s' % yaml_file
+    print(msg)
+
