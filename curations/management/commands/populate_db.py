d82e660d8971acce47022f62a4cf0d1bfe53c94d
Gary Chang <gwh.chang@gmail.com>
2015-05-28 15:24:42 -0500

    parser

--- curations/management/commands/populate_db.py
+++ curations/management/commands/populate_db.py
@@ -1,6 +1,8 @@
 from django.core.management.base import BaseCommand, CommandError
 from curations.models import Collection, Asset
 
+import yaml
+
 class Command(BaseCommand):
   help = 'Populates Collection, Asset db from yaml file'
   
@@ -12,3 +14,11 @@ class Command(BaseCommand):
     msg = 'Parsing yaml file %s' % yaml_file
     print(msg)
 
+    yaml_text = open(yaml_file).read()
+    yaml_data = yaml.load(yaml_text, yaml.SafeLoader)
+    if 'content' in yaml_data:
+      contents = yaml_data['content']
+      for c in contents:
+        print(c['title'])
+    else:
+      print("Yaml missing 'content' tag.")
