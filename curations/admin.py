8dc9cb1f50b7ee93d070822a3c8db00ff79ccee9
Gary Chang <gwh.chang@gmail.com>
2015-05-28 15:48:29 -0500

    populate db

--- curations/admin.py
+++ curations/admin.py
@@ -3,6 +3,10 @@ from django.contrib import admin
 from .models import Collection, Asset
 
 # Register your models here.
-admin.site.register(Collection)
+
+class CollectionAdmin(admin.ModelAdmin):
+  list_display = ('title_text','author_text','description_text')
+
+admin.site.register(Collection, CollectionAdmin)
 admin.site.register(Asset)
 
--- curations/management/commands/populate_db.py
+++ curations/management/commands/populate_db.py
@@ -10,6 +10,11 @@ class Command(BaseCommand):
     parser.add_argument('yaml_file', type=str)
 
   def handle(self, *args, **options):
+    # Delete all records
+    Collection.objects.all().delete()
+    print('Delete all records in Collection.')
+
+    # Parse yaml file
     yaml_file = options['yaml_file']
     msg = 'Parsing yaml file %s' % yaml_file
     print(msg)
@@ -19,6 +24,22 @@ class Command(BaseCommand):
     if 'content' in yaml_data:
       contents = yaml_data['content']
       for c in contents:
-        print(c['title'])
+        self.handle_collection(c)
     else:
       print("Yaml missing 'content' tag.")
+
+  def handle_collection(self, node):
+    title = node['title']
+    print(title)
+
+    if Collection.objects.filter(title_text=title).exists():
+      print("%s already exists in db." % title)
+    else:
+      c = Collection(
+          title_text        = node['title'],
+          author_text       = node['by'], 
+          description_text  = node['description'],
+          homepage_url      = node['link'])
+      c.save()
+
+      print(c.id)
