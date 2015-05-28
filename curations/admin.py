f866b3a3424e8dcef8ca13e9d6210228a59272bd
Gary Chang <gwh.chang@gmail.com>
2015-05-28 17:36:36 -0500

    admin

--- curations/admin.py
+++ curations/admin.py
@@ -4,8 +4,13 @@ from .models import Collection, Asset
 
 # Register your models here.
 
+class AssetInline(admin.TabularInline):
+  model = Asset
+  extra = 3
+
 class CollectionAdmin(admin.ModelAdmin):
   list_display = ('title_text','author_text','description_text')
+  inlines = [AssetInline]
 
 admin.site.register(Collection, CollectionAdmin)
 admin.site.register(Asset)
